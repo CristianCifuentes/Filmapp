fa147aed1523a4198b0d23f1b9306e26a0a39e5f
cristian.cifuentes3@o365.unab.edu.co <cristian.cifuentes3@o364.unab.edu.co>
2022-11-17 17:12:15 -0500

    Segundo update

--- app/src/main/java/com/filmapp/filmapp/MainActivity.java
+++ app/src/main/java/com/filmapp/filmapp/MainActivity.java
@@ -1,14 +1,57 @@
 package com.filmapp.filmapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+
+        //Animaciones
+        Animation animacion1= AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2= AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView FilmappTextView= findViewById(R.id.FilmappTextView);
+        ImageView LogoImageView=findViewById(R.id.LogoImageView);
+
+        FilmappTextView.setAnimation(animacion2);
+        LogoImageView.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+                Pair[] pairs=new Pair[2];
+                pairs[0] =new Pair<View, String>(LogoImageView, "logoImagenTrans");
+                pairs[1] =new Pair<View, String>(FilmappTextView, "textTrans");
+
+                if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+                    ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                    startActivity(intent, options.toBundle());
+                }else {
+                    startActivity(intent);
+                    finish();
+                }
+               // startActivity(intent);
+               // finish();
+            }
+        }, 4000);
     }
 }
