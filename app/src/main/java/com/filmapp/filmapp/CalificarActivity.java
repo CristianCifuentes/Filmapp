ae903715113e1884bfa1c10bad3a02c5499947bf
cristian.cifuentes3@o365.unab.edu.co <cristian.cifuentes3@o364.unab.edu.co>
2022-11-17 18:29:04 -0500

    Tercer update

---
+++ app/src/main/java/com/filmapp/filmapp/CalificarActivity.java
@@ -0,0 +1,19 @@
+package com.filmapp.filmapp;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class CalificarActivity extends AppCompatActivity {
+
+    private RatingBar ratingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calificar);
+
+    }
+}
